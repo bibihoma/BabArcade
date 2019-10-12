9aeeb45ab7c51832cd442e2f2df06bca5d32cdc1
bibihoma <colin.gaumont@gmail.com>
2019-10-11 22:32:57 -0400

    still starting

--- Babarcadator.py
+++ Babarcadator.py
@@ -1,8 +1,10 @@
 from gpiozero.pins.mock import MockFactory
 from gpiozero import Device, Button, LED
 from time import sleep
+import signal
+
 import logging
-import Game
+from Game import *
 # Set the default pin factory to a mock factory
 logging.basicConfig(level=logging.DEBUG,format='%(funcName)s:%(lineno)d:%(message)s')
 Device.pin_factory = MockFactory()
@@ -11,6 +13,11 @@ Device.pin_factory = MockFactory()
 # devices
 led = LED(17)
 btn = Button(16)
+btns[colors.BLUE][pos.BACK]=Button(10)
+btns[colors.BLUE][pos.FRONT]=Button(11)
+btns[colors.RED][pos.BACK]=Button(12)
+btns[colors.RED][pos.FRONT]=Button(13)
+
 led.source = btn
 logging.info('Lets go')
 
@@ -20,6 +27,11 @@ logging.debug(led.value)
 
 # Get a reference to mock pin 16 (used by the button)
 btn_pin = Device.pin_factory.pin(16)
+mockBtn[colors.BLUE][pos.BACK]=Device.pin_factory.pin(10)
+mockBtn[colors.BLUE][pos.FRONT]=Device.pin_factory.pin(11)
+mockBtn[colors.RED][pos.BACK]=Device.pin_factory.pin(12)
+mockBtn[colors.RED][pos.FRONT]=Device.pin_factory.pin(13)
+
 
 # Drive the pin low (this is what would happen electrically when the button is
 # pushed)
@@ -33,4 +45,11 @@ logging.debug(btn.value)
 btn_pin.drive_low()
 logging.debug(btn.value)
 
+game = Game()
+# Simulate RFID
+game.registerPlayer("Jerem",colors.RED,pos.FRONT)
+game.registerPlayer("Etienne",colors.RED,pos.BACK)
+game.registerPlayer("Colin",colors.BLUE,pos.FRONT)
+game.registerPlayer("Thomas",colors.BLUE,pos.BACK)
 
+signal.pause()
--- Game.py
+++ Game.py
@@ -1,4 +1,10 @@
+import logging
 from enum import Enum
+
+class teams(Enum):
+    T1 =1
+    T2= 2
+
 class colors (Enum):
     BLUE= 1
     RED= 2
@@ -6,7 +12,7 @@ class colors (Enum):
 class pos (Enum):
     FRONT= 1
     BACK = 2
-    AUTOGOAL = 3
+    GOALFROMDEFENSE = 3
     NEUTRAL = 4
 
 class gameStatus(Enum):
@@ -14,18 +20,22 @@ class gameStatus(Enum):
     STARTED = 2
     OVER = 3
 
-class player:
+class Player:
     def __init__(self,playername,color, position):
             self._playername = playername
             self._color = color
             self._position = position
+            self._team = "unassigned"
 
-class game:
+    def __str__(self):
+        return (self._playername + " " + self._team + " " + self._color +" " + self._position )
+
+class Game:
     def __init__(self):
         self._players = []
         self._score = []
         self._goalEvents = []
-        self._status = gameStatus.STARTED
+        self._status = gameStatus.WAITINGPlayers
 
     def startSet(self):
         logging.info("Start set")
@@ -37,12 +47,23 @@ class game:
         logging.info("uploading to google")
 
     def registerPlayer(self,playerName,color,position):
+        Player(playerName, color, position)
+        if Player(playerName,color,position) in self._players:
+                logging.error('Player Already registered')
+                return False
+
         if self._status != gameStatus.WAITINGPlayers:
             logging.error('Trying to register a 5th player')
+
         logging.info(playerName + ' joined the game')
         self._players.append(Player(playerName,color,position))
         if len(self._players) == 4:
             logging.info('Game starting')
+            for player in self._players:
+                if player._color == colors.BLUE:
+                        player._team = teams.T1
+                elif player._color== colors.RED:
+                        player._team = teams.T2
             self._status = gameStatus.STARTED
 
     def rollback(self):
@@ -51,47 +72,71 @@ class game:
     def score(self,player):
         if self._status != gameStatus.STARTED:
             logging.warning('Trying to score before game start')
-        logging.info('Gooalll')
+        logging.info('Gooalll, adding goal event, then computing score')
         self._goalEvents.append(player)
+        self.ComputeScore()
+
+        return
 
     def ComputeScore(self):
-        currentSet = 1
-        self._score[currentSet][colors.BLUE] = 0
-        self._score[currentSet][colors.RED] = 0
-        self._manches[colors.BLUE] = 0
-        self._manches[colors.RED] = 0
+        self._currentSet = 1
+        self._score[self._currentSet][teams.T1] = 0
+        self._score[self._currentSet][teams.T2] = 0
+        self._manches[teams.T1] = 0
+        self._manches[teams.T2] = 0
         lastEvent = ""
 
         for scorer in self._goalEvents:
-            if scorer._color == colors.RED:
-                self._score[currentSet][colors.RED] += 1
-            if scorer._color == colors.BLUE:
-                self._score[currentSet][colors.BLUE] += 1
-                lastEvent = ' encore un but'
-            if self._score[currentSet][colors.BLUE] == 10 or self._score[currentSet][colors.RED] == 10 :
-                logging.debug('Set won by ' + scorer._color)
-                lastEvent = 'Set won by ' + scorer._color
-                self._manches[scorer._color]+= 1
+            self._score[self._currentSet][scorer._team] += 1
+            lastEvent = ' encore un but'
 
-                for color in colors:
-                        if self._manches[color] == 2:
+            if self._score[self._currentSet][teams.T1] == 10 or self._score[self._currentSet][teams.T2] == 10 :
+                logging.debug('Set won by ' + scorer._color + scorer._team)
+                lastEvent = 'Set won by ' + scorer._color + scorer._team
+                self._manches[scorer._team]+= 1
+
+                for team in teams:
+                        if len(self._manches[team]) == 2:
                             logging.debug('Game won by ' + scorer._color)
                             lastEvent = 'Game won by ' + scorer._color
                             self._status = gameStatus.OVER
+                            return
+
 
                 else:
                     logging.debug('Starting a new set')
-                    currentSet += 1
-                    self._score[currentSet][colors.BLUE] = 0
-                    self._score[currentSet][colors.RED] = 0
+                    self.swapTeamsColors()
+                    self._currentSet += 1
+                    self._score[self._currentSet][teams.T1] = 0
+                    self._score[self._currentSet][teams.T2] = 0
+            else:
+                if self._score[self._currentSet][scorer._team]%2 == 0:
+                    self.swapPlayersPosition(scorer._team)
 
         return lastEvent
 
     def autogoal(self,color):
         logging.info('Autogoal')
 
-    def swapPlayerPosition(self,color):
+    def swapPlayersPosition(self,team):
         logging.info('Swapping players position')
+        for player in self._players:
+            if player._team == team:
+                if player._position == pos.FRONT:
+                    player._position = pos.BACK
+                elif player._position ==pos.BACK:
+                    player._position = pos.FRONT
+                logging.info('position after player position swap '+ player)
+
+    def swapTeamsColors(self):
+        logging.info('Swapping teams colors')
+        for player in self._players:
+            if player._color == colors.RED:
+                    player._color = colors.BLUE
+            if player._color == colors.BLUE:
+                    player._color=colors.RED
+            logging.info("after swap " + player)
+
 
     def __str__(self):
         return (str(self._players)+str(self._score))
